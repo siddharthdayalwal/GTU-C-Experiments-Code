e8975573fc184a82f18a529d0d2438cf92ec69a0
Siddharth Dayalwal <siddharthdayalwal2000@gmail.com>
2019-08-15 11:24:23 +0530

    Create Type_Of_Character.cpp

---
+++ Practical_Set_2/Type_Of_Character.cpp
@@ -0,0 +1,21 @@
+/*   CHECKING TYPE OF CHARACTER INPUT   */
+#include<stdio.h>
+#include<conio.h>
+#define p printf
+#define s scanf
+void main()
+{
+clrscr();
+char a;
+p("\n Enter any character ::");
+s("%c",&a);
+if(a>='A' && a<='Z')
+p("\n %c is a capital letter.",a);
+else if(a>='a' && a<='z')
+p("\n %c is a small letter.",a);
+else if(a>='0' && a<='9')
+p("\n %c is a digit.",a);
+else
+p("\n %c is a special character.",a);
+getch();
+}
