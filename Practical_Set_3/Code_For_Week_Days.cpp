d408117363465c843b05c8b2f20f3d764dbd9bb5
Siddharth Dayalwal <siddharthdayalwal2000@gmail.com>
2019-08-16 19:42:56 +0530

    Create Code_For_Week_Days.cpp

---
+++ Practical_Set_3/Code_For_Week_Days.cpp
@@ -0,0 +1,55 @@
+/*   NUMBER CODE FOR WEEK DAYS   */
+#include<stdio.h>
+#include<conio.h>
+#define p printf
+#define s scanf
+void main()
+{
+  clrscr();
+  int a;
+  p("Select DAY CODE from the following::\n\n Sunday   ::1\n Monday   ::2\n Tuesday  ::3\n Wednesday::4\n Thursday ::5\n Friday   ::6\n Saturday ::7\n\n Enter the code::");
+  s("%d",&a);
+  switch(a)
+  {
+    case 1:
+    {
+	    p("\n It's a Sunday today.");
+    	break;
+    }
+    case 2:
+    {
+	    p("\n It's a Monday today.");
+	    break;
+    }
+    case 3:
+    {	
+      p("\n It's a Tuesday today.");
+	    break;
+    }
+    case 4:
+	  {
+      p("\n It's a Wednesday today.");
+	    break;
+    }
+    case 5:
+	  {
+      p("\n It's a Thursday today.");
+	    break;
+    }
+    case 6:
+	  {
+      p("\n It's a Friday today.");
+	    break;
+    }
+    case 7:
+	  {
+      p("\n It's a Saturday today.");
+	    break;
+    }
+    default:
+	  {  
+      p("\n Not a Valid DAY_CODE.");
+    }
+  }
+  getch();
+}
