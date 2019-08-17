52f549b590e5cf2449cd242a79a37cc3b7e33661
Siddharth Dayalwal <siddharthdayalwal2000@gmail.com>
2019-08-17 18:53:47 +0530

    Update Sum_Of_First_And_Last_Digit_Of_A_Number.cpp

--- Practical_Set_4/Sum_Of_First_And_Last_Digit_Of_A_Number.cpp
+++ Practical_Set_4/Sum_Of_First_And_Last_Digit_Of_A_Number.cpp
@@ -6,14 +6,14 @@
 #define s scanf
 void main()
 {
-clrscr();
-int a,n,f,l,sum;
-p("\n Enter any number");
-s("%d",&n);
-l=n%10;
-a=log10(n);
-f=n/pow(10,a);
-sum=l+f;
-p("\n Sum of first digit and last digit of %d is %d",n,sum);
-getch();
+  clrscr();
+  int a,n,f,l,sum;
+  p("\n Enter any number");
+  s("%d",&n);
+  l=n%10;
+  a=log10(n);
+  f=n/pow(10,a);
+  sum=l+f;
+  p("\n Sum of first digit and last digit of %d is %d",n,sum);
+  getch();
 }
