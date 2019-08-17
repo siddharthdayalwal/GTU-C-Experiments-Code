a5494e719293d6b6f6c578e01b6af331c3f712c9
Siddharth Dayalwal <siddharthdayalwal2000@gmail.com>
2019-08-17 18:34:56 +0530

    Create Max_Min_From_10_Numbers.cpp

---
+++ Practical_Set_3/Max_Min_From_10_Numbers.cpp
@@ -0,0 +1,30 @@
+/*   MAXIMUM AND MINIMUM NUMBER OUT OF 10 NUMBER   */
+#include<stdio.h>
+#include<conio.h>
+#define p printf
+#define s scanf
+void main()
+{
+  clrscr();
+  int i,n,max,min;
+  p("Enter the value.1 ::");
+  s("%d",&n);
+  max=n;
+  min=n;
+  for(i=2;i<=10;i++)
+  {
+	  p("Enter the value.%d ::",i);
+	  s("%d",&n);
+	  if(n>max)
+	  {
+      max=n;
+    }
+	  if(n<min)
+	  {
+      min=n;
+    }
+  }
+  p("\n Maximum number is %d",max);
+  p("\n Minimum number is %d",min);
+  getch();
+}
