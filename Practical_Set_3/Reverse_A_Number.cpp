7fdf28dcefe73caef8befb9f42d82b10ca5db4c1
Siddharth Dayalwal <siddharthdayalwal2000@gmail.com>
2019-08-17 18:44:57 +0530

    Create Reverse_A_Number.cpp

---
+++ Practical_Set_3/Reverse_A_Number.cpp
@@ -0,0 +1,23 @@
+/*   REVERSE OF A NUMBER   */
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#define p printf
+#define s scanf
+void main()
+{
+clrscr();
+long int a,i,n,sum=0,b,x;
+p("\n Enter the number ::");
+s("%ld",&x);
+a=x;
+n=log10(a)+1;
+for(i=1;i<=n;i++)
+{
+	b=a%10;
+	sum=sum*10+b;
+	a/=10;
+}
+p("\n REVERSE of %ld is %ld",x,sum);
+getch();
+}
