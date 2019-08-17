a6e0e77462344a4e90c1057837aa45f14c566109
Siddharth Dayalwal <siddharthdayalwal2000@gmail.com>
2019-08-17 18:39:23 +0530

    Update Check_Last_Digit_Is_Even_Or_Odd.cpp

--- Practical_Set_3/Check_Last_Digit_Is_Even_Or_Odd.cpp
+++ Practical_Set_3/Check_Last_Digit_Is_Even_Or_Odd.cpp
@@ -5,14 +5,14 @@
 #define s scanf
 void main()
 {
-clrscr();
-long int a,f=1,i;
-p("\n Enter the number ::");
-s("%ld",&a);
-for(i=1;i<=a;i++)
-{
-	f*=i;
-}
-p("\n %ld! = %ld",a,f);
-getch();
+	clrscr();
+	long int a,f=1,i;
+	p("\n Enter the number ::");
+	s("%ld",&a);
+	for(i=1;i<=a;i++)
+	{
+		f*=i;
+	}
+	p("\n %ld! = %ld",a,f);
+	getch();
 }
