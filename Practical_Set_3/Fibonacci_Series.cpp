c132f9155a61a70ff0918687819f987bea3f0e6c
Siddharth Dayalwal <siddharthdayalwal2000@gmail.com>
2019-08-17 18:47:34 +0530

    Update Fibonacci_Series.cpp

--- Practical_Set_3/Fibonacci_Series.cpp
+++ Practical_Set_3/Fibonacci_Series.cpp
@@ -5,23 +5,23 @@
 #define s scanf
 void main()
 {
-clrscr();
-int n,i,a=0,b=1,c;
-p("\n Enter no. of terms of fibonacci series :: ");
-s("%d",&n);
-if(n==1)
-p("\n %d ",a);
-if(n>1)
-{
+	clrscr();
+	int n,i,a=0,b=1,c;
+	p("\n Enter no. of terms of fibonacci series :: ");
+	s("%d",&n);
+	if(n==1)
 	p("\n %d ",a);
-	p("%d ",b);
-}
-for(i=3;i<=n;i++)
-{
-	c=a+b;
-	p("%d ",c);
-	a=b;
-	b=c;
-}
-getch();
+	if(n>1)
+	{
+		p("\n %d ",a);
+		p("%d ",b);
+	}
+	for(i=3;i<=n;i++)
+	{
+		c=a+b;
+		p("%d ",c);
+		a=b;
+		b=c;
+	}
+	getch();
 }
