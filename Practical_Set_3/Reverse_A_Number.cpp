7dcb1075c3ffad7b2161345db2c5b8d9326d206a
Siddharth Dayalwal <siddharthdayalwal2000@gmail.com>
2019-08-17 18:45:57 +0530

    Update Reverse_A_Number.cpp

--- Practical_Set_3/Reverse_A_Number.cpp
+++ Practical_Set_3/Reverse_A_Number.cpp
@@ -6,18 +6,18 @@
 #define s scanf
 void main()
 {
-clrscr();
-long int a,i,n,sum=0,b,x;
-p("\n Enter the number ::");
-s("%ld",&x);
-a=x;
-n=log10(a)+1;
-for(i=1;i<=n;i++)
-{
-	b=a%10;
-	sum=sum*10+b;
-	a/=10;
-}
-p("\n REVERSE of %ld is %ld",x,sum);
-getch();
+	clrscr();
+	long int a,i,n,sum=0,b,x;
+	p("\n Enter the number ::");
+	s("%ld",&x);
+	a=x;
+	n=log10(a)+1;
+	for(i=1;i<=n;i++)
+	{
+		b=a%10;
+		sum=sum*10+b;
+		a/=10;
+	}
+	p("\n REVERSE of %ld is %ld",x,sum);
+	getch();
 }
