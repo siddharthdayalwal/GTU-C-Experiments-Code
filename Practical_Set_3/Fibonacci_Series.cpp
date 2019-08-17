057aef3d25d95f2a30678a3cfe7a39f72554ea0d
Siddharth Dayalwal <siddharthdayalwal2000@gmail.com>
2019-08-17 18:46:48 +0530

    Create Fibonacci_Series.cpp

---
+++ Practical_Set_3/Fibonacci_Series.cpp
@@ -0,0 +1,27 @@
+/*   FIBONACCI SERIES   */
+#include<stdio.h>
+#include<conio.h>
+#define p printf
+#define s scanf
+void main()
+{
+clrscr();
+int n,i,a=0,b=1,c;
+p("\n Enter no. of terms of fibonacci series :: ");
+s("%d",&n);
+if(n==1)
+p("\n %d ",a);
+if(n>1)
+{
+	p("\n %d ",a);
+	p("%d ",b);
+}
+for(i=3;i<=n;i++)
+{
+	c=a+b;
+	p("%d ",c);
+	a=b;
+	b=c;
+}
+getch();
+}
