ea4508cd9e1bf6717ff31ae5ab04d34afb8d4871
Siddharth Dayalwal <siddharthdayalwal2000@gmail.com>
2019-08-18 11:58:14 +0530

    Update Check_Prime_Number.cpp

--- Practical_Set_4/Check_Prime_Number.cpp
+++ Practical_Set_4/Check_Prime_Number.cpp
@@ -5,18 +5,24 @@
 #define s scanf
 void main()
 {
-clrscr();
-int n,i,c;
-p("\n Enter any number :: ");
-s("%d",&n);
-for(i=1,c=0;i<=n;i++)
-{
-	if(n%i==0)
-	c++;
-}
-if(c==2)
-p("\n %d is a prime number",n);
-else
-p("\n %d is not a prime number",n);
-getch();
+	clrscr();
+	int n,i,c;
+	p("\n Enter any number :: ");
+	s("%d",&n);
+	for(i=1,c=0;i<=n;i++)
+	{
+		if(n%i==0)
+		{
+			c++;
+		}
+	}
+	if(c==2)
+	{
+		p("\n %d is a prime number",n);
+	}
+	else
+	{
+		p("\n %d is not a prime number",n);
+	}
+	getch();
 }
