c8b36b6171625fd86f78f1c55466f8e532361ab6
Siddharth Dayalwal <siddharthdayalwal2000@gmail.com>
2019-08-18 12:10:30 +0530

    Update Series_1+1\2!+1\3!+...+1\n!.cpp

--- Practical_Set_5/Series_1+1\2!+1\3!+...+1\n!.cpp
+++ Practical_Set_5/Series_1+1\2!+1\3!+...+1\n!.cpp
@@ -1,22 +1,24 @@
 /*   EVALUATING SERIES 1 + 1/2! + 1/3! +...+ 1/n!   */
+/*     In the filename I have used backslash ( \ ) instead of divide ( / )      */
+
 #include<stdio.h>
 #include<conio.h>
 #define p printf
 #define s scanf
 void main()
 {
-clrscr();
-int i,n,f;
-float sum;
-p("\n Enter the number of terms :: ");
-s("%d",&n);
-p("\n");
-for(i=1,sum=0,f=1;i<=n;i++)
-{
-	f*=i;
-	sum+=1.0/f;
-	p("+ 1/%d! ",i);
-}
-p(" = %f",sum);
-getch();
+	clrscr();
+	int i,n,f;
+	float sum;
+	p("\n Enter the number of terms :: ");
+	s("%d",&n);
+	p("\n");
+	for(i=1,sum=0,f=1;i<=n;i++)
+	{
+		f*=i;
+		sum+=1.0/f;
+		p("+ 1/%d! ",i);
+	}
+	p(" = %f",sum);
+	getch();
 }
