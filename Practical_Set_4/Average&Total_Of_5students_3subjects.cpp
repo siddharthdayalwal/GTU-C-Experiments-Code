e39b7657d4b299626ad0926e0b8d00e7348fd80c
Siddharth Dayalwal <siddharthdayalwal2000@gmail.com>
2019-08-18 11:48:30 +0530

    Update Average&Total_Of_5students_3subjects.cpp

--- Practical_Set_4/Average&Total_Of_5students_3subjects.cpp
+++ Practical_Set_4/Average&Total_Of_5students_3subjects.cpp
@@ -5,21 +5,21 @@
 #define s scanf
 void main()
 {
-clrscr();
-int i,j,m,t=0;
-float a;
-for(i=1;i<=5;i++)
-{
-	p("\n \nEnter marks of student.%d :: \n",i);
-	for(j=1,t=0;j<=3;j++)
+	clrscr();
+	int i,j,m,t=0;
+	float a;
+	for(i=1;i<=5;i++)
 	{
-		p("Marks.%d :: ",j);
-		s("%d",&m);
-		t+=m;
+		p("\n \nEnter marks of student.%d :: \n",i);
+		for(j=1,t=0;j<=3;j++)
+		{
+			p("Marks.%d :: ",j);
+			s("%d",&m);
+			t+=m;
+		}
+		a=t/3.0;
+		p("Total marks of student.%d = %d",i,t);
+		p("\nAvg marks of student.%d = %f",i,a);
 	}
-	a=t/3.0;
-	p("Total marks of student.%d = %d",i,t);
-	p("\nAvg marks of student.%d = %f",i,a);
-}
-getch();
+	getch();
 }
