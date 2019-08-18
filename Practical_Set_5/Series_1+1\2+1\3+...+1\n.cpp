9ff271b406263511ba674c0824398fa9055d50fc
Siddharth Dayalwal <siddharthdayalwal2000@gmail.com>
2019-08-18 12:05:39 +0530

    Create Series_1+1\2+1\3+...+1\n.cpp
    In the name of the file I have used backslash ( \ ) instead of divide ( / ).

---
+++ Practical_Set_5/Series_1+1\2+1\3+...+1\n.cpp
@@ -0,0 +1,21 @@
+/*   EVALUATING SERIES 1 + 1/2 + 1/3 +...+ 1/n   */
+#include<stdio.h>
+#include<conio.h>
+#define p printf
+#define s scanf
+void main()
+{
+clrscr();
+int i,n;
+float sum;
+p("\n Enter the number of terms :: ");
+s("%d",&n);
+p("\n");
+for(i=1,sum=0;i<=n;i++)
+{
+	sum+=1.0/i;
+	p("+ 1/%d ",i);
+}
+p(" = %f",sum);
+getch();
+}
