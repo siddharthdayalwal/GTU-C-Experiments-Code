0c02a88d09962c8a7ebbe8ae15dc27eac7b284a8
Siddharth Dayalwal <siddharthdayalwal2000@gmail.com>
2019-08-18 12:13:13 +0530

    Update Series_1-x+x^2\2!+...-x^9\9!.cpp

--- Practical_Set_5/Series_1-x+x^2\2!+...-x^9\9!.cpp
+++ Practical_Set_5/Series_1-x+x^2\2!+...-x^9\9!.cpp
@@ -1,4 +1,3 @@
-
 /*   EVALUATING SERIES 1-x+x^2/2!+...-x^9/9!   */
 /*     In the filename I have used backslash ( \ ) instead of divide ( / )     */
 
@@ -9,27 +8,27 @@
 #define s scanf
 void main()
 {
-clrscr();
-int i,n,f;
-float sum;
-p("\n Enter the value of x :: ");
-s("%d",&n);
-p("\n");
-p("\n 1");
-for(i=1,sum=1,f=1;i<=9;i++)
-{
-	f*=i;
-	if(i%2!=0)
+	clrscr();
+	int i,n,f;
+	float sum;
+	p("\n Enter the value of x :: ");
+	s("%d",&n);
+	p("\n");
+	p("\n 1");
+	for(i=1,sum=1,f=1;i<=9;i++)
 	{
-		sum-=(pow(n,i))/float(f);
-		p("-%d^%d/%d!",n,i,i);
+		f*=i;
+		if(i%2!=0)
+		{
+			sum-=(pow(n,i))/float(f);
+			p("-%d^%d/%d!",n,i,i);
+		}
+		else
+		{
+			sum+=(pow(n,i))/float(f);
+			p("+%d^%d/%d!",n,i,i);
+		}
 	}
-	else
-	{
-		sum+=(pow(n,i))/float(f);
-		p("+%d^%d/%d!",n,i,i);
-	}
-}
-p(" = %f",sum);
-getch();
+	p(" = %f",sum);
+	getch();
 }
