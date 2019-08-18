f47d362cef6690cfddb96a8860bb886440900c6d
Siddharth Dayalwal <siddharthdayalwal2000@gmail.com>
2019-08-18 12:09:26 +0530

    Create Series_1+1\2!+1\3!+...+1\n!.cpp

---
+++ Practical_Set_5/Series_1+1\2!+1\3!+...+1\n!.cpp
@@ -0,0 +1,22 @@
+/*   EVALUATING SERIES 1 + 1/2! + 1/3! +...+ 1/n!   */
+#include<stdio.h>
+#include<conio.h>
+#define p printf
+#define s scanf
+void main()
+{
+clrscr();
+int i,n,f;
+float sum;
+p("\n Enter the number of terms :: ");
+s("%d",&n);
+p("\n");
+for(i=1,sum=0,f=1;i<=n;i++)
+{
+	f*=i;
+	sum+=1.0/f;
+	p("+ 1/%d! ",i);
+}
+p(" = %f",sum);
+getch();
+}
