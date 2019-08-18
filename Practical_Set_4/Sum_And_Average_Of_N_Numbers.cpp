bd9accde17d0d61fe957d278354cdc9150c5ff53
Siddharth Dayalwal <siddharthdayalwal2000@gmail.com>
2019-08-18 11:46:29 +0530

    Update Sum_And_Average_Of_N_Numbers.cpp

--- Practical_Set_4/Sum_And_Average_Of_N_Numbers.cpp
+++ Practical_Set_4/Sum_And_Average_Of_N_Numbers.cpp
@@ -5,20 +5,20 @@
 #define s scanf
 void main()
 {
-clrscr();
-int i,a,sum=0;
-float avg,n;
-p("Enter no. of terms ::");
-s("%f",&n);
-p("\n");
-for(i=1;i<=n;i++)
-{
-	p("Enter value.%d :: ",i);
-	s("%d",&a);
-	sum+=a;
-}
-avg=sum/n;
-p("\n SUM = %d",sum);
-p("\n AVG = %f",avg);
-getch();
+	clrscr();
+	int i,a,sum=0;
+	float avg,n;
+	p("Enter no. of terms ::");
+	s("%f",&n);	
+	p("\n");
+	for(i=1;i<=n;i++)
+	{
+		p("Enter value.%d :: ",i);
+		s("%d",&a);
+		sum+=a;
+	}
+	avg=sum/n;
+	p("\n SUM = %d",sum);
+	p("\n AVG = %f",avg);
+	getch();
 }
