fe8427f90260e93d9d4b86948336c488ce0abe1b
Siddharth Dayalwal <siddharthdayalwal2000@gmail.com>
2019-08-18 12:00:55 +0530

    Create Series_1^2+2^2+...+n^2.cpp

---
+++ Practical_Set_5/Series_1^2+2^2+...+n^2.cpp
@@ -0,0 +1,20 @@
+/*   EVALUATING SERIES 1^2+2^2+...+n^2   */
+#include<stdio.h>
+#include<conio.h>
+#define p printf
+#define s scanf
+void main()
+{
+clrscr();
+int sum,i,n;
+p("\n Enter the number of terms :: ");
+s("%d",&n);
+p("\n");
+for(i=1,sum=0;i<=n;i++)
+{
+	sum+=i*i;
+	p("+ %d^2 ",i);
+}
+p(" = %d",sum);
+getch();
+}
