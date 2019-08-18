2b36645eb9e4c5733d48d28daa23f17e7458f828
Siddharth Dayalwal <siddharthdayalwal2000@gmail.com>
2019-08-18 11:44:27 +0530

    Create Sum_And_Average_Of_N_Numbers.cpp

---
+++ Practical_Set_4/Sum_And_Average_Of_N_Numbers.cpp
@@ -0,0 +1,24 @@
+/*   SUM AND AVERAGE OF NO. OF TERMS USER WANTS   */
+#include<stdio.h>
+#include<conio.h>
+#define p printf
+#define s scanf
+void main()
+{
+clrscr();
+int i,a,sum=0;
+float avg,n;
+p("Enter no. of terms ::");
+s("%f",&n);
+p("\n");
+for(i=1;i<=n;i++)
+{
+	p("Enter value.%d :: ",i);
+	s("%d",&a);
+	sum+=a;
+}
+avg=sum/n;
+p("\n SUM = %d",sum);
+p("\n AVG = %f",avg);
+getch();
+}
